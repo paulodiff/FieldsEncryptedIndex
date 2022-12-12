793965780fb2adbe8adc0e395434f132074885bd
RUGGERI Ruggero <M05831@comune.rimini.it>
2022-12-12 12:12:16 +0100

    12/12/2022-12:12:16,79

---
+++ GIT-UPDATE.bat
@@ -0,0 +1,12 @@
+REM GIT TUTORIAL .gitignore (files and folder to skip)
+REM 1) git init
+REM 2) git remote add origin https://github.com/paulodiff/FieldsEncryptedIndex.git
+
+REM git pull origin master 
+
+ECHO GIT-UPDATE
+
+
+git add .
+git commit -m "%DATE%-%TIME%"
+git push origin master
